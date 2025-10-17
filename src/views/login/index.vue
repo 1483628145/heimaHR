656884de237107193ce67be39c98b2fde4ac045e
laixiaobo <1483628145l@gmail.com>
2025-10-17 13:56:55 +0800

    完成登录页面的静态结构

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,20 +8,14 @@
       <el-card shadow="never" class="login-card">
         <!--登录表单-->
         <!-- el-form > el-form-item > el-input -->
-        <el-form ref="form" :model="loginForm" :rules="loginRules">
-          <el-form-item prop="mobile">
-            <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input v-model="loginForm.password" show-password placeholder="请输入密码" />
-          </el-form-item>
-          <el-form-item prop="isAgree">
-            <el-checkbox v-model="loginForm.isAgree">
-              用户平台使用协议
-            </el-checkbox>
+        <el-form>
+          <el-form-item>
+            <el-input placeholder="请输入手机号"></el-input>
+            <el-input style="margin-top: 10px;" placeholder="请输入密码"></el-input>
+            <el-checkbox>用户使用协议</el-checkbox>
           </el-form-item>
           <el-form-item>
-            <el-button style="width: 350px" type="primary" @click="login">登录</el-button>
+            <el-button style="width: 350px;" type="primary">登录</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -33,65 +27,11 @@ export default {
   name: "Login",
   data() {
     return {
-      // 登录表单数据
-      loginForm: {
-        mobile: "13800000002",
-        password: "itHeiMa@20251016",
-        isAgree: true,
-      },
-      // 登录表单校验规则
-      loginRules: {
-        mobile: [
-          {
-            required: true,
-            message: "请输入手机号",
-            trigger: "blur",
-          },
-          {
-            pattern: /^1[3-9]\d{9}$/,
-            message: "手机号格式不正确",
-            trigger: "blur",
-          },
-        ],
-        password: [
-          {
-            required: true,
-            message: "请输入密码",
-            trigger: "blur",
-          },
-          {
-            min: 6,
-            max: 16,
-            message: "密码长度应该为6-16位之间",
-            trigger: "blur",
-          },
-        ],
-        // required只能检测 null undefined ""
-        isAgree: [
-          {
-            validator: (rule, value, callback) => {
-              // rule校验规则
-              // value 校验的值
-              // callback 函数 - promise resolve reject
-              // callback() callback(new Error(错误信息))
-              value
-                ? callback()
-                : callback(new Error("您必须勾选用户的使用协议"));
-            },
-          },
-        ],
-      },
+
     };
   },
   methods: {
-    login() {
-      this.$refs.form.validate((isOK) => {
-        if (isOK) {
-          // alert("校验通过");
-          // 校验通过开始登录 -- 登录保存token 然后跳转到dashboard
-        }
-      });
-    },
+
   },
 };
 </script>
