e4a89779fb0fd4c8253be680950fdef97d2920b2
laixiaobo <1483628145l@gmail.com>
2025-10-19 12:08:03 +0800

    完成修改密码静态页面的搭建

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -13,19 +13,44 @@
           <span>{{ username }}</span>
           <i class="el-icon-setting" />
         </div>
-        <!-- 弹窗 -->
+        <!-- 下拉菜单 -->
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
             <el-dropdown-item>
               主页
             </el-dropdown-item>
           </router-link>
+          <el-dropdown-item divided>
+            <span style="display:block;" @click="updatePassword">修改密码</span>
+          </el-dropdown-item>
           <el-dropdown-item divided @click.native="logout">
             <span style="display:block;">退出登录</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </div>
+
+
+    <!-- 修改密码 对话框 -->
+    <!-- 注意这个弹出层需要放置在最外层div的上一层级 -->
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form :model="updatePasswordForm" ref="updatePasswdRef" :rules="updatePasswdRules">
+        <el-form-item label="原密码:" prop="oldPassword">
+          <el-input v-model="updatePasswordForm.oldPassword"></el-input>
+        </el-form-item>
+        <el-form-item label="新密码:" prop="newPassword">
+          <el-input v-model="updatePasswordForm.newPassword"></el-input>
+        </el-form-item>
+        <el-form-item label="确认新密码:" prop="confirmNewPassword">
+          <el-input v-model="updatePasswordForm.confirmNewPassword"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button>取 消</el-button>
+        <el-button type="primary" @click="confiremUpdatePassword">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -35,6 +60,63 @@ import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 
 export default {
+  data() {
+    return {
+      // 弹出层
+      dialogFormVisible: true,
+      // 更新密码表单
+      updatePasswordForm: {
+        oldPassword: '',
+        newPassword: '',
+        confirmNewPassword: ''
+      },
+      // 密码校验规则
+      updatePasswdRules: {
+        oldPassword: [
+          {
+            required: true,
+            message: '请输入原密码！',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 12,
+            message: '密码的长度为6-12',
+            trigger: 'blur'
+
+          }
+        ],
+        newPassword: [
+          {
+            required: true,
+            message: '请输入新密码！',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 12,
+            message: '密码的长度为6-12',
+            trigger: 'blur'
+
+          }
+        ],
+        confirmNewPassword: [
+          {
+            required: true,
+            message: '请再次输入新密码！',
+            trigger: 'blur',
+          },
+          {
+            min: 6,
+            max: 12,
+            message: '密码的长度为6-12',
+            trigger: 'blur'
+
+          }
+        ]
+      }
+    }
+  },
   components: {
     Breadcrumb,
     Hamburger
@@ -53,6 +135,18 @@ export default {
     async logout() {
       await this.$store.dispatch('user/logout')
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+    },
+    // 修改密码
+    updatePassword() {
+      // 打开弹出层
+      this.dialogFormVisible = true
+    },
+    // 确认修改密码
+    confiremUpdatePassword() {
+      // 表单验证
+
+      // 调用接口修改密码
+      // 
     }
   }
 }
