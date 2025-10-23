e55170f32f79f511b6dc6cdddd66b5492527545d
laixiaobo <1483628145l@gmail.com>
2025-10-23 09:42:56 +0800

    作用域插槽接取数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <el-tree :data="departmentList" :props="defaultProps"></el-tree>
+      <!-- 树形结构 -->
+      <el-tree :data="departmentList" :props="defaultProps">
+        <!-- 使用vue模板作为数据传递给下面 -->
+        <template v-slot="{ data }">
+          <el-row style="width: 100%; height: 40px;" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="manager">{{ data.managerName }}</span>
+              <el-dropdown>
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑部门</el-dropdown-item>
+                  <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -11,7 +32,15 @@ export default {
   name: 'department',
   data() {
     return {
-      departmentList: [],
+      departmentList: [
+        {
+          name: '创智教育',
+          managerName: '管理员',
+          children: [
+            { name: '张三', managerName: "李四" }
+          ]
+        }
+      ],
       defaultProps: {
         children: 'children',
         label: 'name'
@@ -31,4 +60,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.manager {
+  margin-right: 10px;
+}
+</style>
