e62c8bbe06b5fd0308eb4f17cc7bea95101ec586
laixiaobo <1483628145l@gmail.com>
2025-10-23 10:26:49 +0800

    列表数据转层级数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <!-- 树形结构 -->
-      <el-tree :data="departmentList" :props="defaultProps">
-        <!-- 使用vue模板作为数据传递给下面 -->
+      <!-- 展示树形结构 -->
+      <el-tree :expand-on-click-node="false" default-expand-all :data="departmentList" :props="defaultProps">
+        <!-- 节点结构 -->
+        <!-- v-slot="{ node, data }" 只能作用在template -->
         <template v-slot="{ data }">
-          <el-row style="width: 100%; height: 40px;" type="flex" justify="space-between" align="middle">
+          <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
             <el-col>{{ data.name }}</el-col>
             <el-col :span="4">
-              <span class="manager">{{ data.managerName }}</span>
-              <el-dropdown>
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <!-- $event 实参 表示类型 -->
+              <el-dropdown @command="operateDept($event, data.id)">
+                <!-- 显示区域内容 -->
                 <span class="el-dropdown-link">
-                  操作<i class="el-icon-arrow-down el-icon--right"></i>
+                  操作<i class="el-icon-arrow-down el-icon--right" />
                 </span>
+                <!-- 下拉菜单选项 -->
                 <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                  <el-dropdown-item>编辑部门</el-dropdown-item>
-                  <el-dropdown-item>删除</el-dropdown-item>
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
             </el-col>
@@ -28,29 +32,26 @@
 </template>
 <script>
 import { getDepartmentList } from '@/api/department'
+import { ListToTreeData } from '@/utils'
 export default {
   name: 'department',
   data() {
     return {
-      departmentList: [
-        {
-          name: '创智教育',
-          managerName: '管理员',
-          children: [
-            { name: '张三', managerName: "李四" }
-          ]
-        }
-      ],
+      departmentList: [],
       defaultProps: {
         children: 'children',
         label: 'name'
       }
     }
   },
+
   methods: {
     async getList() {
       const res = await getDepartmentList()
-      console.log(res);
+      ListToTreeData(res, 0)
+      this.departmentList = res
+      console.log(this.departmentList);
+
     },
 
   },
@@ -61,6 +62,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.app-container {
+  padding: 50px;
+}
+
 .manager {
   margin-right: 10px;
 }
